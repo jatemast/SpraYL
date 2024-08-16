913b297e120620d2672dc33285c360bd1a7a9294
javiertm-2023 <programador14@tecnologicocomfenalco.edu.co>
2024-08-16 14:28:54 -0500

    ya s e almacena el tipo de carro

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\seleccioncarro;
+use Illuminate\Support\Facades\Validator;
 
 class bookingController extends Controller
 {
     public function store(Request $request)
     {
-           // Validar los datos de la solicitud
-           $validatedData = $request->validate([
+
+        $validation = Validator::make($request->all(), [
             'marca' => 'required|string|max:255',
             'modelo' => 'required|string|max:255',
-            'anio' => 'required|string|max:4',
+            'anio' => 'required|string|max:255',
             'color' => 'required|string|max:255',
+
         ]);
 
+
+        if($validation->fails()){
+            return response()->json($validation->errors(), 400);
+        }
+
         // Crear un nuevo registro en la base de datos usando el modelo
-        $carro = seleccioncarro::create($validatedData);
+        $carro = seleccioncarro::create($request->all());
 
         // Responder con éxito
         return response()->json(['message' => 'Carro almacenado exitosamente', 'data' => $carro], 201);
