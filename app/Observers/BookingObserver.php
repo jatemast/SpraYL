64ddd185316f0f591b3c8466d45db7b89b9bce5f
Edw. Glz <dev.zanntech@outlook.com>
2024-08-17 22:37:10 -0600

    feat: (Observable)

---
+++ app/Observers/BookingObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\HelloMail;
+use App\Models\Booking;
+use Illuminate\Support\Facades\Mail;
+
+class BookingObserver
+{
+    /**
+     * Handle the Booking "created" event.
+     */
+    public function created(Booking $booking): void
+    {
+        $mail_client = $booking->email_cliente;
+        Mail::to($mail_client)->send(new HelloMail());
+    }
+
+    /**
+     * Handle the Booking "updated" event.
+     */
+    public function updated(Booking $booking): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Booking "deleted" event.
+     */
+    public function deleted(Booking $booking): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Booking "restored" event.
+     */
+    public function restored(Booking $booking): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Booking "force deleted" event.
+     */
+    public function forceDeleted(Booking $booking): void
+    {
+        //
+    }
+}
