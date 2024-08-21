f980b7adfc8573ed27b2cc51be275a720a14dde5
javiertm-2023 <programador14@tecnologicocomfenalco.edu.co>
2024-08-21 17:35:03 -0500

    mailer final

--- resources/views/emails/hello.blade.php
+++ resources/views/emails/hello.blade.php
@@ -46,12 +46,13 @@
             <h4>Nuevo Booking Registrado</h4>
         </div>
         <div class="ticket-body">
-            <!-- Eliminar campos Nombre, Fecha, Marca y Modelo -->
-            <p><strong>Año:</strong> {{ $booking->anio }}</p>
+            <!-- Eliminar campos Nombre, Apellido, Año y Fecha -->
+            <p class="hidden"><strong>Nombre del Cliente:</strong> {{ $booking->nombre_cliente }}</p>
+            <p class="hidden"><strong>Apellido del Cliente:</strong> {{ $booking->apellido_cliente }}</p>
+            <p class="hidden"><strong>Año:</strong> {{ $booking->anio }}</p>
+            <p class="hidden"><strong>Fecha del Booking:</strong> {{ $booking->fecha }}</p>
             <p><strong>Color:</strong> {{ $booking->color }}</p>
             <p><strong>Servicio Extra:</strong> {{ $booking->servicio_extra }}</p>
-            <p><strong>Nombre del Cliente:</strong> {{ $booking->nombre_cliente }}</p>
-            <p><strong>Apellido del Cliente:</strong> {{ $booking->apellido_cliente }}</p>
             <p><strong>Email del Cliente:</strong> {{ $booking->email_cliente }}</p>
             <p><strong>Teléfono del Cliente:</strong> {{ $booking->telefono_cliente }}</p>
             <p><strong>Dirección del Cliente:</strong> {{ $booking->direccion_cliente }}</p>
